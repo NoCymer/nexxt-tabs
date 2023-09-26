96408111d42166a3c80543834dcbf92776b1d22e
NoCymer <61873313+NoCymer@users.noreply.github.com>
2023-09-26 10:21:50 +0200

    Update BackgroundsDatabase.test.ts

--- tests/BackgroundsDatabase.test.ts
+++ tests/BackgroundsDatabase.test.ts
@@ -1,6 +1,10 @@
 import backgroundsDB from '../src/BackgroundsDatabase';
 
 describe("Testing BackgroundDatabase class", () => {
+    test("Storing & Fetching", async () => {
+        await backgroundsDB.storeBackground("0", "dummy");
+        await backgroundsDB.storeBackground("1", "test1");
+    })
     test("FetchAll", async () => {
         expect(await backgroundsDB.fetchIds()).toStrictEqual(["0","1"]);
     })
